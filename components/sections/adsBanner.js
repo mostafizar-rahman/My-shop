092d5b1fdd48f7450fabfb6b0557930f87fcb2bc
mostafizar-rahman <mostafizurdh2000@gmail.com>
2024-07-08 21:58:27 +0600

    update ads banner

--- components/sections/adsBanner.js
+++ components/sections/adsBanner.js
@@ -1,29 +1,23 @@
+import Image from "next/image";
 import React from "react";
-
+import shoes_1 from "@/assets/images/man-fashion/shoes-1.png"
 const AdsBanner = () => {
     return (
         <div className="max-w-[1900px] mx-auto">
-            <div className=" py-6  mt-10" style={{background: `linear-gradient(135deg, #46DD82, #63C52E)`}}>
+            <div className=" py-6  mt-10" style={{ background: `linear-gradient(135deg, #46DD82, #63C52E)` }}>
                 <div className="lg:max-w-[1280px] md:max-w-[696px] max-w-[343px] mx-auto ">
                     <div className="lg:flex md:flex block justify-between items-center">
                         <div className="md:p-0 p-4 ">
-                            <p className="text-base leading-none text-white">
-                                Save upto 30%
-                            </p>
-                            <p className="text-3xl font-semibold leading-9 text-white py-4">
-                                Summer Sale
-                            </p>
-                            <p className="text-base leading-normal text-white">
+                            <p className="text-base leading-none text-white bg-orange-500 inline-block px-4 py-2 rounded-md">Save upto 30%</p>
+                            <h2 className="text-9xl font-semibold leading-none text-white py-4 ">Summer Sale</h2>
+                            <p className="leading-normal text-white text-lg font-norican">
                                 Want to save some cash and still look like a fashion diva ?
                                 <br />
                                 Checkout our summer sale now !!!
                             </p>
                         </div>
                         <div className="md:p-0 p-4">
-                            <img
-                                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/ec2.png"
-                                className="w-full h-full"
-                            />
+                            <Image src={shoes_1} width={400} height={500} className="w-full h-full" />
                         </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
+import React from 'react'
 import Head from 'next/head'
 
 import { productsData } from '@/utlits/productsData'
 
-import AdsBanner from '@/components/AdsBanner/AdsBanner'
+
 import Banner from '@/components/sections/banner';
 import ImportantProducts from '@/components/sections/importantProducts';
 import MainCard from '@/components/ui/card/mainCard';
 import HomeCategoryCard from '@/components/sections/homeCategoryCard';
 import PartnerLogo from '@/components/sections/partnerLogo';
-
-
-
-
+import AdsBanner from '@/components/sections/adsBanner';
 
 
 const Home = ({ data }) => {
-  const [screenSize, setScreenSize] = useState(1100)
-
-
-  useEffect(() => {
-    function handleResize() {
-      setScreenSize(window.innerWidth);
-    }
-
-
-    window.addEventListener('resize', handleResize);
-
-
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, [])
-
-  // -- Responsive swipper js
-  let perView = 4
-  if (screenSize <= 1100) {
-    perView = 4
-  }
-  if (screenSize <= 1000) {
-    perView = 3
-  }
-  if (screenSize <= 767) {
-    perView = 2
-  }
-  if (screenSize <= 500) {
-    perView = 1
-  }
-
-  let perView2 = 7
-  if (screenSize <= 1100) {
-    perView2 = 6
-  }
-  if (screenSize <= 1000) {
-    perView2 = 5
-  }
-  if (screenSize <= 767) {
-    perView2 = 3
-  }
-  if (screenSize <= 500) {
-    perView2 = 2
-  }
-
-
-
   return (
     <section>
       <Head>
@@ -96,7 +45,7 @@ const Home = ({ data }) => {
         </div>
       </div>
       {/* ------ Brands */}
-     <PartnerLogo/>
+      <PartnerLogo />
 
     </section>
   )
